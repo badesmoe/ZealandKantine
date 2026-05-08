c704f0b2a634c9ede6a726323850cf25f6a6750b
Simon Bade Moe <simon@bade.dk>
2026-05-08 10:28:44 +0200

    Tilføjet IRepository<T>

---
+++ ZealandKantine/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ZealandKantine.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+       void Create(T entity);
+       List<T> ReadAll();
+       T Read(int id);
+       void Update(T entity);
+       void Delete(int id);
+    }
+}
