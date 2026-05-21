6cdfa2d08a3b753c9d77e303531593f2e3e119f5
Martin Alejandro Hernandez Quintana <mart01f8@edu.zealand.dk>
2026-05-21 13:06:51 +0200

    Add monthly statements page and navigation updates
    Added a new Razor Page `GenerateMonthlyStatement.cshtml` to display
    monthly statements with filtering options for month and year.
    Introduced `MyStatementsModel` in `MyStatements.cshtml.cs` to handle
    data retrieval, including integration with `UserService` for user ID
    resolution. Updated `_Layout.cshtml` to include navigation links to
    the new page in both the main navigation bar and admin dropdown menu.

--- ZealandKantine/Pages/MonthlyStatements/MyStatements.cshtml.cs
+++ ZealandKantine/Pages/MonthlyStatements/MyStatements.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ZealandKantine.Models;
 using ZealandKantine.Repositories;
+using ZealandKantine.Services;
 
 namespace ZealandKantine.Pages.MonthlyStatements
 {
@@ -9,17 +10,19 @@ namespace ZealandKantine.Pages.MonthlyStatements
     public class MyStatementsModel : PageModel
     {
         private readonly MonthlyStatementRepository _monthlyStatementRepository;
+        private readonly UserService _userService;
 
         public List<MonthlyStatement> Statements { get; set; }
 
-        public MyStatementsModel(MonthlyStatementRepository monthlyStatementRepository)
+        public MyStatementsModel(MonthlyStatementRepository monthlyStatementRepository, UserService userService)
         {
             _monthlyStatementRepository = monthlyStatementRepository;
+            _userService = userService;
         }
 
         public void OnGet()
         {
-            int userId = int.Parse(User.FindFirst("UserId").Value);
+            int userId = _userService.GetUserIdByName(User.Identity.Name) ?? 0;
             Statements = _monthlyStatementRepository.GetByUserId(userId);
         }
     }
