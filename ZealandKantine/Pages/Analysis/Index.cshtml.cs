d1ab993d0e99fc9ecd2490b4f2531a7d794a471c
Christoffer <bendigsunne@hotmail.com>
2026-05-26 10:56:57 +0200

    can now filter search per quater with a button press.

--- ZealandKantine/Pages/Analysis/Index.cshtml.cs
+++ ZealandKantine/Pages/Analysis/Index.cshtml.cs
@@ -13,6 +13,8 @@ namespace ZealandKantine.Pages.Analysis
         public DateTime StartDate { get; set; } = DateTime.Today.AddDays(-1);
         [BindProperty(SupportsGet = true)]
         public DateTime EndDate { get; set; } = DateTime.Today;
+        [BindProperty(SupportsGet = true)]
+        public int? Quarter { get; set; }
 
         public List<(MenuItem MenuItem, int TotalSold)> SoldMenuItems { get; set; }
         public List<(DailySpecial DailySpecial, int TotalSold)> SoldDailySpecials { get; set; }
@@ -27,6 +29,26 @@ namespace ZealandKantine.Pages.Analysis
 
         public void OnGet()
         {
+            if (Quarter.HasValue)
+            {
+                var year = DateTime.Today.Year;
+                StartDate = Quarter.Value switch
+                {
+                    1 => new DateTime(year, 1, 1),
+                    2 => new DateTime(year, 4, 1),
+                    3 => new DateTime(year, 7, 1),
+                    4 => new DateTime(year, 10, 1),
+                    _ => StartDate
+                };
+                EndDate = Quarter.Value switch
+                {
+                    1 => new DateTime(year, 3, 31),
+                    2 => new DateTime(year, 6, 30),
+                    3 => new DateTime(year, 9, 30),
+                    4 => new DateTime(year, 12, 31),
+                    _ => EndDate
+                };
+            }
             SoldMenuItems = _analysisService.GetMenuItemsSortedBySales();
             SoldDailySpecials = _analysisService.GetDailySpecialsSortedBySales();
             RevenueByDate = _analysisService.GetRevenueByDate(StartDate, EndDate);
